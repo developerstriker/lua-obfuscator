79c3ccc1e4638edb981aa0f9046a8f581269ff5d
Striker <strikerdev@iMac-Pro-de-Eduardo.local>
2026-04-13 13:53:29 -0300

    Fix: add --recursive to clone prometheus submodule

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ echo "Downloading src/fxbuild.lua..."
 curl -sL "$BASE_URL/src/fxbuild.lua" -o src/fxbuild.lua
 
 echo "Downloading prometheus submodule..."
-git clone --depth 1 https://github.com/developerstriker/lua-obfuscator.git tmp_prometheus
+git clone --depth 1 --recursive https://github.com/developerstriker/lua-obfuscator.git tmp_prometheus
 mv tmp_prometheus/prometheus .
 rm -rf tmp_prometheus
 
