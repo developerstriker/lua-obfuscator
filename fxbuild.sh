5484333e0742e55ab701c0a95d73093d2990bf39
Striker <strikerdev@iMac-Pro-de-Eduardo.local>
2026-04-13 13:54:25 -0300

    Fix: clone prometheus from prometheus-lua repo

--- fxbuild.sh
+++ fxbuild.sh
@@ -4,8 +4,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR"
 
 if [ ! -d "prometheus" ]; then
-    echo "Cloning prometheus submodule..."
-    git submodule update --init --recursive
+    echo "Cloning prometheus obfuscator..."
+    git clone --depth 1 https://github.com/prometheus-lua/Prometheus.git prometheus
 fi
 
 luajit "$DIR/src/fxbuild.lua" "$@"
